4494723a270869131d42918ae01a9dbaa38348e9
mhmdfmi <muhamadfahmi3240@gmail.com>
2025-07-02 01:20:45 +0700

    Refactor variable name for clarity in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
-const URL = process.env.HOST || "http://localhost";
+const HOST = process.env.HOST || "http://localhost";
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running at ${URL}:${PORT}`);
+  console.log(`Server running at ${HOST}:${PORT}`);
 });
